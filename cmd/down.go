d2593b8eef47ae2d260bed6768a9da3a5d5aee70
John Ritsema <john_ritsema@yahoo.com>
2016-07-07 16:56:31 -0400

    Initial implementation of the up command (closes #1)

--- cmd/down.go
+++ cmd/down.go
@@ -26,7 +26,7 @@ func down(cmd *cobra.Command, args []string) {
 	var harborCompose = DeserializeHarborCompose(HarborComposeFile)
 
 	//read the docker compose file
-	var dockerCompose = DeserializeDockerCompose(DockerComposeFile)
+	dockerCompose := DeserializeDockerCompose(DockerComposeFile)
 	if dockerCompose.Version != "2" {
 		log.Fatal("only docker-compose format v2 is supported")
 	}
@@ -39,7 +39,7 @@ func down(cmd *cobra.Command, args []string) {
 	//prompt for password
 	fmt.Printf("Password: ")
 	passwd, _ := gopass.GetPasswd()
-	var pass = string(passwd)
+	pass := string(passwd)
 
 	//authenticate and get token
 	var token = GetToken(User, pass)
@@ -49,7 +49,7 @@ func down(cmd *cobra.Command, args []string) {
 
 	//iterate shipments
 	for shipmentName, shipment := range harborCompose.Shipments {
-		fmt.Println("Stopping " + shipmentName)
+		fmt.Printf("Stopping %v ...\n", shipmentName)
 
 		if Verbose {
 			log.Println("processing  " + shipmentName + "/" + shipment.Env)
