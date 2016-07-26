74035f50228e243fcec7b0c30db87eb0a58d7062
Michael Smith <smithatlanta@gmail.com>
2016-07-26 11:11:51 -0400

    Use godep for dependency management.  Closes #8
    Use the [godep](https://github.com/tools/godep) tool to managed dependencies by including the /vendor code in the repo.

--- cmd/helmitTypes.go
+++ cmd/helmitTypes.go
@@ -2,22 +2,22 @@ package cmd
 
 // HelmitContainer represents a single contianer instance in harbor
 type HelmitContainer struct {
-	Name string `json:"name"`
-	Id string `json:"id"`
-	Image string `json:"image"`
-	Log_stream string `json:"log_stream"`
-	Logs []string `json:"logs"`
+	Name      string   `json:"name"`
+	ID        string   `json:"id"`
+	Image     string   `json:"image"`
+	Logstream string   `json:"log_stream"`
+	Logs      []string `json:"logs"`
 }
 
 // HelmitReplica represents a single running replica in harbor
 type HelmitReplica struct {
-	Host string `json:"host"`
-	Provider string `json:"provider"`
+	Host       string            `json:"host"`
+	Provider   string            `json:"provider"`
 	Containers []HelmitContainer `json:"containers"`
 }
 
 // HelmitResponse represents a response from helmit
 type HelmitResponse struct {
-	Error bool `json:"error"`
+	Error    bool            `json:"error"`
 	Replicas []HelmitReplica `json:"replicas"`
 }
--- cmd/logs.go
+++ cmd/logs.go
@@ -1,28 +1,30 @@
 package cmd
 
 import (
-		"fmt"
-		"strings"
-	  "time"
-		"sort"
-		"encoding/json"
-		"github.com/spf13/cobra"
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
 )
 
-var Time bool
-var Separate bool
+var logTime bool
+var separate bool
+
 // logsCmd represents the logs command
 var logsCmd = &cobra.Command{
-		Use:   "logs",
-		Short: "View output from containers",
-		Long:  ``,
-		Run: logs,
+	Use:   "logs",
+	Short: "View output from containers",
+	Long:  ``,
+	Run:   logs,
 }
 
 func init() {
-	  logsCmd.PersistentFlags().BoolVarP(&Time, "time", "t", false, "append time to logs")
-		logsCmd.PersistentFlags().BoolVarP(&Separate, "separate", "s", false, "print logs by each container")
-		RootCmd.AddCommand(logsCmd)
+	logsCmd.PersistentFlags().BoolVarP(&logTime, "time", "t", false, "append time to logs")
+	logsCmd.PersistentFlags().BoolVarP(&separate, "separate", "s", false, "print logs by each container")
+	RootCmd.AddCommand(logsCmd)
 }
 
 // logs cli command
@@ -30,40 +32,42 @@ func init() {
 // Flags: -t: adds time to the logs
 // TODO: add the rest of the flags to match docker-compose
 func logs(cmd *cobra.Command, args []string) {
-		//read the harbor compose file
-		var harborCompose = DeserializeHarborCompose(HarborComposeFile)
-		//iterate shipments
-		for shipmentName, shipment := range harborCompose.Shipments {
-			fmt.Println("Logs For:  " + shipmentName + " " + shipment.Env)
-			helmitObject := HelmitResponse{}
-			var response = GetLogs(shipment.Barge, shipmentName, shipment.Env)
-			err := json.Unmarshal([]byte(response), &helmitObject)
-			if err != nil {
-			    fmt.Println(err)
-			}
-
-			if Separate == true {
-			    printSeparateLogs(helmitObject)
-			} else {
-          printMergedLogs(helmitObject)
-			}
+	//read the harbor compose file
+	var harborCompose = DeserializeHarborCompose(HarborComposeFile)
+	//iterate shipments
+	for shipmentName, shipment := range harborCompose.Shipments {
+		fmt.Println("Logs For:  " + shipmentName + " " + shipment.Env)
+		helmitObject := HelmitResponse{}
+		var response = GetLogs(shipment.Barge, shipmentName, shipment.Env)
+		err := json.Unmarshal([]byte(response), &helmitObject)
+		if err != nil {
+			fmt.Println(err)
 		}
+
+		if separate == true {
+			printSeparateLogs(helmitObject)
+		} else {
+			printMergedLogs(helmitObject)
+		}
+	}
 }
 
-// Object that contains a containers logs
-type LogsObject struct {
-	Name string
-	Id string
+// logsObject that contains a containers logs
+type logsObject struct {
+	Name  string
+	ID    string
 	Image string
-	Logs Logs
+	Logs  Logs
 }
 
-type LogObject struct {
+// logObject is a log object
+type logObject struct {
 	Time time.Time
-	Log string
+	Log  string
 }
 
-type Logs []LogObject
+// Logs is a list
+type Logs []logObject
 
 func (slice Logs) Len() int {
 	return len(slice)
@@ -79,53 +83,53 @@ func (slice Logs) Swap(i, j int) {
 
 func printMergedLogs(shipment HelmitResponse) {
 	layout := "2006-01-02T15:04:05.999999999Z"
-	shipmentLogs := make([]LogsObject, len(shipment.Replicas))
+	shipmentLogs := make([]logsObject, len(shipment.Replicas))
 	for _, provider := range shipment.Replicas {
-			for _, container := range provider.Containers {
-				  var containerLogs = Logs{}
-					for _, log := range container.Logs {
-							line := strings.Fields(log)
-							timeValue, err := time.Parse(layout, line[0])
-							if err != nil {
-									timeValue, err = time.Parse(layout, line[0][:1])
-									if err != nil {
-											continue
-									}
-							}
-
-							var logObject = LogObject{}
-
-							logObject.Time = timeValue
-							logObject.Log = strings.Join(line, " ")
-
-							containerLogs = append(containerLogs, logObject)
+		for _, container := range provider.Containers {
+			var containerLogs = Logs{}
+			for _, log := range container.Logs {
+				line := strings.Fields(log)
+				timeValue, err := time.Parse(layout, line[0])
+				if err != nil {
+					timeValue, err = time.Parse(layout, line[0][:1])
+					if err != nil {
+						continue
 					}
-					var logsObject  = LogsObject{}
-					logsObject.Name = container.Name
-					logsObject.Id = container.Id
-					logsObject.Image = container.Image
-					logsObject.Logs = containerLogs
-					shipmentLogs = append(shipmentLogs, logsObject)
+				}
+
+				var logObject = logObject{}
+
+				logObject.Time = timeValue
+				logObject.Log = strings.Join(line, " ")
+
+				containerLogs = append(containerLogs, logObject)
 			}
+			var logsObject = logsObject{}
+			logsObject.Name = container.Name
+			logsObject.ID = container.ID
+			logsObject.Image = container.Image
+			logsObject.Logs = containerLogs
+			shipmentLogs = append(shipmentLogs, logsObject)
+		}
 	}
 
 	var mergedLogs Logs
 	for _, logObject := range shipmentLogs {
 		for _, logObj := range logObject.Logs {
-			  newLog := logObject.Name + ":" + logObject.Id[0:5] + "  | "
-				if Time == true {
-				     newLog = newLog + logObj.Time.String()  + ", "
-				}
+			newLog := logObject.Name + ":" + logObject.ID[0:5] + "  | "
+			if logTime == true {
+				newLog = newLog + logObj.Time.String() + ", "
+			}
 
-				logObj.Log = newLog + logObj.Log + "\n"
-			  mergedLogs = append(mergedLogs, logObj)
+			logObj.Log = newLog + logObj.Log + "\n"
+			mergedLogs = append(mergedLogs, logObj)
 		}
 	}
 
 	sort.Sort(mergedLogs)
 
 	for _, log := range mergedLogs {
-			fmt.Printf(log.Log)
+		fmt.Printf(log.Log)
 	}
 }
 
@@ -133,19 +137,19 @@ func printMergedLogs(shipment HelmitResponse) {
 // prints the logs separatly for each shipment
 func printSeparateLogs(shipment HelmitResponse) {
 	for _, provider := range shipment.Replicas {
-			for _, container := range provider.Containers {
-					fmt.Printf("--- Name: %s\n", container.Name)
-					fmt.Printf("--- Id: %s\n", container.Id)
-					fmt.Printf("--- Image %s\n", container.Image)
-					for _, log := range container.Logs {
-							line := strings.Fields(log)
+		for _, container := range provider.Containers {
+			fmt.Printf("--- Name: %s\n", container.Name)
+			fmt.Printf("--- Id: %s\n", container.ID)
+			fmt.Printf("--- Image %s\n", container.Image)
+			for _, log := range container.Logs {
+				line := strings.Fields(log)
 
-							if Time == false {
-								line = append(line[:0], line[1:]...)
-							}
+				if logTime == false {
+					line = append(line[:0], line[1:]...)
+				}
 
-							fmt.Println(strings.Join(line, " "))
-					}
+				fmt.Println(strings.Join(line, " "))
 			}
+		}
 	}
 }
