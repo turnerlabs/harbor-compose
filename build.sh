eed8b475f085a16080c2ff6becf24c72676032ab
John Ritsema <john.ritsema@turner.com>
2016-10-28 10:23:41 -0400

    fixed build after ghr stopped working with go 1.6 by downloading the released binary

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ BUILD_VERSION=$(git describe --tags)
 BUILD_DATE=$(date -u '+%Y/%m/%d-%H:%M:%S')
 
 echo building $BUILD_VERSION $BUILD_DATE
-gox -ldflags "-X main.version=$BUILD_VERSION -X main.buildDate=$BUILD_DATE" -output "dist/ncd_{{.OS}}_{{.Arch}}"
+gox -osarch="darwin/amd64" -osarch="linux/amd64" -osarch="windows/amd64" -ldflags "-X main.version=$BUILD_VERSION -X main.buildDate=$BUILD_DATE" -output "dist/ncd_{{.OS}}_{{.Arch}}"
